c65c597e90f904fbf437c93e5001dc61adeb4db0
udaydevs <udaysinghno2005@gmail.com>
2025-12-24 12:52:50 +0530

    improving responsiveness

--- src/components/ui/pattern.jsx
+++ src/components/ui/pattern.jsx
@@ -1,14 +1,14 @@
 export default function Pattern(){
     return(
-        <div className="absolute top-26 flex z-2 w-[80%] h-[65%] opacity-25  ">
+        <div className="absolute top-26 hidden  lg:flex z-2 w-[80%] h-[65%] opacity-25  ">
                     <div className="h-full w-[33%] p-4">
                         <div className="h-1/2 w-full flex m-0 p-0">
-                            <div className="w-1/2 h-full border-b-[2px]  border-r-[2px] bg-transparent rounded-br-4xl"></div>
-                            <div className="w-1/2 h-full border-b-[2px]  border-l-[2px] bg-transparent rounded-bl-4xl"></div>
+                            <div className="w-1/2 h-full border-b-2  border-r-2 bg-transparent rounded-br-4xl"></div>
+                            <div className="w-1/2 h-full border-b-2  border-l-2 bg-transparent rounded-bl-4xl"></div>
                         </div>
                         <div className="h-1/2 w-full flex m-0 p-0">
-                            <div className="w-1/2 h-full border-t-[2px]   border-r-[2px] rounded-tr-4xl"></div>
-                            <div className="w-1/2 h-full border-t-[2px]  border-l-[2px] rounded-tl-4xl"></div>
+                            <div className="w-1/2 h-full border-t-2   border-r-2 rounded-tr-4xl"></div>
+                            <div className="w-1/2 h-full border-t-2  border-l-2 rounded-tl-4xl"></div>
                         </div>
                     </div>
 
@@ -17,12 +17,12 @@ export default function Pattern(){
 
                     <div className="h-full w-[33%] p-4 ">
                         <div className="h-1/2 w-full flex m-0 p-0">
-                            <div className="w-1/2 h-full border-b-[2px]  border-r-[2px] bg-transparent rounded-br-4xl"></div>
-                            <div className="w-1/2 h-full border-b-[2px]  border-l-[2px] bg-transparent rounded-bl-4xl"></div>
+                            <div className="w-1/2 h-full border-b-2  border-r-2 bg-transparent rounded-br-4xl"></div>
+                            <div className="w-1/2 h-full border-b-2 border-l-2 bg-transparent rounded-bl-4xl"></div>
                         </div>
                         <div className="h-1/2 w-full flex m-0 p-0">
-                            <div className="w-1/2 h-full border-t-[2px]   border-r-[2px] rounded-tr-4xl"></div>
-                            <div className="w-1/2 h-full border-t-[2px]  border-l-[2px] rounded-tl-4xl"></div>
+                            <div className="w-1/2 h-full border-t-2   border-r-2 rounded-tr-4xl"></div>
+                            <div className="w-1/2 h-full border-t-2  border-l-2 rounded-tl-4xl"></div>
                         </div>
                     </div>
                 </div>
