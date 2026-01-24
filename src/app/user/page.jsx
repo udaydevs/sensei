3ee521164d0b78c6925a5a8f18cfb732ea77260f
udaydevs <udaysinghno2005@gmail.com>
2026-01-25 00:29:46 +0530

    Working on new chatbot feature

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,23 +1,22 @@
 'use client'
 import { useState, useEffect, Children } from "react";
 import remarkGfm from "remark-gfm";
-import rehypeRaw from "rehype-raw";
-import rehypeSanitize from "rehype-sanitize";
-import remarkBreaks from "remark-breaks";
-import Markdown from "react-markdown";
+import ReactMarkdown from "react-markdown"
+
 
 function App() {
   const [input, setInput] = useState("");
+  const [resp, setResp] = useState('')
   const [messages, setMessages] = useState([]);
   const [ws, setws] = useState(null);
 
   useEffect(() => {
-    const websocket = new WebSocket('ws://127.0.0.1:8000/response/');
+    const websocket = new WebSocket('ws://127.0.0.1:8000/chatbot/talk');
     setws(websocket)
 
     websocket.onopen = () => console.log('ws opened');
     websocket.onmessage = (event) => {
-      setMessages((prevMessage) => [...prevMessage, event.data]);
+      setResp((prevMessage) => (prevMessage + event.data));
     }
     websocket.onclose = () => console.log('ws.closed')
 
@@ -25,38 +24,53 @@ function App() {
   }, []);
 
   const sendPrompt = () => {
-    if (ws && ws.readyState == WebSocket.OPEN){
+    if (ws && ws.readyState == WebSocket.OPEN) {
       ws.send(input)
+      setMessages((prev) => [...prev, resp]);
+      setResp('')
       setInput('')
     }
   };
 
   return (
-    <div style={styles.container} className="text-left">
+    <div className="h-screen flex flex-col items-center p-5 bg-purple-100">
       <h1>LLM Live Streaming</h1>
+      <div className="bg-red-500 p-1">
+        {messages && messages.map((item, index) => (
+          <div key={index} className="my-2 p-4 rounded-xl shadow-md">
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>
+              {item}
+            </ReactMarkdown>
+          </div>
+        ))}
 
-      {messages.map((value) => (
-        <Markdown
-          remarkPlugins={[remarkBreaks, remarkGfm]}
-          rehypePlugins={[rehypeRaw, rehypeSanitize]} 
-        >{value}</Markdown>
-      ))}
-      {/* <textarea
-        style={styles.textarea}
-        value={messages}
-        readOnly
-        placeholder="AI response will appear here..."
-      /> */}
+        {resp && resp.length > 1 && (
+          <div className="my-2 p-4 rounded-xl shadow-md opacity-80">
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>
+              {resp}
+            </ReactMarkdown>
+          </div>
+        )}
+      </div>
 
-      <div style={styles.inputBox}>
+      {/* <div className=" bg-white overflow-y-auto w-4/5 p-5 h-4/5">
+          {messages.map((value) => (
+            <div className="bg-amber-200 py-1"><Markdown
+            remarkPlugins={[remarkBreaks, remarkGfm]}
+            rehypePlugins={[rehypeRaw, rehypeSanitize]} 
+            >{value}</Markdown></div>
+            
+            ))}
+            </div> */}
+      <div className=" flex">
         <input
-          style={styles.input}
+          className="w-100 border p-3.5 rounded-md outline-0 m-3"
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder="Type your question..."
           onKeyDown={(e) => e.key === "Enter" && sendPrompt()}
         />
-        <button style={styles.button} onClick={sendPrompt}>
+        <button className="border p-3.5 rounded-md" onClick={sendPrompt}>
           Send
         </button>
       </div>
@@ -64,34 +78,5 @@ function App() {
   );
 }
 
-const styles = {
-  container: {
-    maxWidth: "600px",
-    margin: "40px auto",
-    textAlign: "center",
-    fontFamily: "Arial, sans-serif",
-  },
-  textarea: {
-    width: "100%",
-    height: "750px",
-    padding: "10px",
-    marginBottom: "20px",
-    fontSize: "16px",
-  },
-  inputBox: {
-    display: "flex",
-    gap: "10px",
-  },
-  input: {
-    flex: 1,
-    padding: "10px",
-    fontSize: "16px",
-  },
-  button: {
-    padding: "10px 20px",
-    fontSize: "16px",
-    cursor: "pointer",
-  },
-};
 
 export default App;
